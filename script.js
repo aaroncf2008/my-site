d9f5aee486a25d46059b05c98e8183386681f3fe
aaroncf2008 <aaroncf2008@gmail.com>
2021-12-10 17:34:48 -0500

    ewaf
    ewaf

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var input = document.getElementById("terminal-input");
-const commands = ["help", "portfolio", "resume", "contact"];
+const commands = ["help", "portfolio", "resume", "contact", "clear"];
 
 var definitions = {
-    "help": "Portfolio - This command links you to my portfolio\nResume - This command links you to my resume\n Contact - This commands will list ways to contact me",
+    "help": "Portfolio - This command links you to my portfolio\nResume - This command links you to my resume\n Contact - This commands will list ways to contact me\n Clear- Clears the terminal",
     "contact": "Email: aaronfriedtech@gmail.com\nGitHub: aaroncf2008",
 };
 
@@ -17,16 +17,22 @@ input.addEventListener("keyup", function(event) {
     let prefix = "root@aaron:~# " + input.value + "\n";
     if (commands.includes(inputval) == false){
         let notfound = prefix + `${input.value}: command not found\n`;
-        console.log(span.innerHTML)
-        let spanval = span.innerHTML.replace("<br>", '\n');
-        console.log(spanval)
+        let spanval = span.innerText.replace("<br>", '\n');
         span.innerText = spanval + notfound;
     }
     else if (inputval == "resume"){
+        let spanval = span.innerText.replace("<br>", '\n');
+        let prefix = "root@aaron:~# " + input.value + "\n";
+        let final = "Forwarding you to my resume!\n";
         window.open("https://pastebin.com/raw/0A9Utp54");
+        span.innerText = spanval + prefix + final
     }
     else if (inputval == "portfolio"){
+        let spanval = span.innerText.replace("<br>", '\n');
+        let prefix = "root@aaron:~# " + input.value + "\n";
+        let final = "Forwarding you to my portfolio!\n";
         window.open("https://pastebin.com/raw/51WzKCdE");
+        span.innerText = spanval + prefix + final
     }
     else if (inputval == "clear"){
         span.innerText = "";
